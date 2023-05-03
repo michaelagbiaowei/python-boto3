4bccd92f712d9a6a8db47b4b94b20185fbf7b71d
michaelagbiaowei <bryanwalton1997@gmail.com>
2023-05-03 02:56:06 +0100

    updated

--- VPC/delete_vpc.py
+++ VPC/delete_vpc.py
@@ -1,7 +1,39 @@
 import boto3
 
-import boto3
-
 ec2 = boto3.resource('ec2')
-vpc = ec2.Vpc('vpc-0ad2289b8e0a7c4b6')
-vpc.delete()
+DeleteNatGatway = boto3.client("ec2")
+DeleteElasticIp = boto3.client("ec2")
+
+# Delete VPC
+Delete_vpc = ec2.Vpc('vpc-0ad2289b8e0a7c4b6')
+Delete_vpc.delete()
+
+# Detach nat gatway
+DetachNat = DeleteNatGatway.detach_internet_gateway(
+    InternetGatewayId='your_id',
+    VpcId='your_id'
+)
+# Delete nat
+DeleteNat = DeleteNatGatway.delete_nat_gateway(NatGatewayId='your_id')
+
+# Detach Elatic IP
+DetachEIP = DeleteElasticIp.disassociate_address(
+    AssociationId='your_id',
+    PublicIp='your_elastic_ip',
+)
+
+# Delete Route tables
+DeletRouteTable1 = ec2.delete_route_table(
+    RouteTableId='your_id'
+)
+DeletRouteTable2 = ec2.delete_route_table(
+    RouteTableId='your_id'
+)
+
+# DeleteSubnets
+DeleteSubnet1 = ec2.delete_subnet(
+    SubnetId='string',
+)
+DeleteSubnet2 = ec2.delete_subnet(
+    SubnetId='string',
+)
