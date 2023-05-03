2f94e10af4f467e80d1788098c6b2a2614ffbd94
michaelagbiaowei <bryanwalton1997@gmail.com>
2023-05-03 12:30:35 +0100

    updated

--- VPC/delete_vpc.py
+++ VPC/delete_vpc.py
@@ -4,25 +4,26 @@ ec2 = boto3.resource('ec2')
 DeleteNatGatway = boto3.client("ec2")
 DeleteElasticIp = boto3.client("ec2")
 
-# Delete VPC
-Delete_vpc = ec2.Vpc('vpc-0ad2289b8e0a7c4b6')
-Delete_vpc.delete()
-
-# Detach nat gatway
-DetachNat = DeleteNatGatway.detach_internet_gateway(
-    InternetGatewayId='your_id',
-    VpcId='your_id'
-)
 # Delete nat
-DeleteNat = DeleteNatGatway.delete_nat_gateway(NatGatewayId='your_id')
+DeleteNat = DeleteNatGatway.delete_nat_gateway(NatGatewayId='nat-083132f427b06517d')
 
-# Detach Elatic IP
+# # Detach Elatic IP
 DetachEIP = DeleteElasticIp.disassociate_address(
     AssociationId='your_id',
     PublicIp='your_elastic_ip',
 )
 
-# Delete Route tables
+# # Detach and Delete  igw
+DetachIgw = ec2.detach_internet_gateway(
+    InternetGatewayId='your_id',
+    VpcId='vpc-0157aaad35705accc'
+)
+DeleteIgw = ec2.delete_internet_gateway(
+    InternetGatewayId='your_id',
+    VpcId='vpc-0157aaad35705accc'
+)
+
+# # Delete Route tables
 DeletRouteTable1 = ec2.delete_route_table(
     RouteTableId='your_id'
 )
@@ -30,7 +31,7 @@ DeletRouteTable2 = ec2.delete_route_table(
     RouteTableId='your_id'
 )
 
-# DeleteSubnets
+# # DeleteSubnets
 DeleteSubnet1 = ec2.delete_subnet(
     SubnetId='string',
 )
